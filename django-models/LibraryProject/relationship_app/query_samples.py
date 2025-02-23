9d2b74e59df50ca1c675c8578d5585bf237ae177
Airelclyde <timothyochiengdev@gmail.com>
2025-02-23 19:21:50 +0300

    Update query_samples.py
    lib

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from .models import Author, Book, Library
+from .models import Author, Book, Library,librarian
 # Use select_related() to reduce queries for ForeignKey relationships
 def get_books_with_authors():
     return Book.objects.select_related('author').all()
