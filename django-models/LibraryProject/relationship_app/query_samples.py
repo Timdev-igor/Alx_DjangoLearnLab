0c6ae369b5bf1cb36c7a874cbc461d90d6c37acd
Tim <timothyochiengdev@gmail.com>
2025-02-25 12:17:33 +0300

    added a comment

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,23 @@
+#writing the queries as stand alon codes without def
+""""
+from .models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+author_name = "J.K. Rowling"
+author = Author.objects.get(name=author_name)  # Retrieve author object
+books_by_author = Book.objects.filter(author=author)  # Filter books by author
+print(f"Books by {author_name}:", list(books_by_author))
+
+# List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"Books in {library_name}:", list(books_in_library))
+
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(f"Librarian of {library_name}: {librarian}")
+"""
 from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
