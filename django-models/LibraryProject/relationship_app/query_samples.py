dac5e7d68096e810ef57058f3e10426bd57b24f3
Tim <timothyochiengdev@gmail.com>
2025-02-25 11:09:26 +0300

    b

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,9 +6,10 @@ def get_books_by_author(author_name):
 
 # List all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.prefetch_related('books').get(name=library_name)
+    library = Library.objects.get(name=library_name)
     return library.books.all()
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    return Librarian.objects.select_related('library').get(library__name=library_name)
+    library = Library.objects.get(name=library_name)
+    return library.librarian
