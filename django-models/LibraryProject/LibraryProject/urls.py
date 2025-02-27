8046052ac18870332c4821beaa36d6248cd461cb
Tim <timothyochiengdev@gmail.com>
2025-02-27 13:03:15 +0300

    md

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -19,10 +19,16 @@ from django.urls import path
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from relationship_app.views import admin_view, librarian_view, member_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("relationship_app/", include("relationship_app.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", TemplateView.as_view(template_name="authentication/profile.html"), name="profile"), 
+    path("admin_view/", admin_view, name="admin_view"),
+    path("librarian_view/", librarian_view, name="librarian_view"),
+    path("member_view/", member_view, name="member_view"),
 ]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,10 +6,38 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView,TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 # Create your views here.
 from django.shortcuts import render
 from .models import Book , Author
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "roles/admin_view.html")
+
+# Librarian view
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "roles/librarian_view.html")
+
+# Member view
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "roles/member_view.html")
 
 #used some functional based views
 def list_books(request):
