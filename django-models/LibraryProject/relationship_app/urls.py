427d5329ea39777352ba7c3e36a99db5051b666a
Tim <timothyochiengdev@gmail.com>
2025-02-27 17:53:31 +0300

    lnkl

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,18 +6,15 @@ from .views import LibraryDetailView, SignUpView
 from . import views  
 from django.contrib.auth import views as auth_views
 
-from django.urls import path
-from .views import LibraryDetailView, list_books, registerView, CustomLoginView, CustomLogoutView
-from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     path('books/', list_books, name='list_books'),
-    path("register/", registerView.as_view(), name="register"),
-    
-    # Use custom login and logout views
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("register/", views.registerView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="authentication/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="authentication/logout.html"), name="logout"),
+
+   
+    path("signup/", SignUpView.as_view(), name="signup"),
 
     # Password Reset Views
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
@@ -25,7 +22,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-    # Password Change Views
+    # Password Change
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
