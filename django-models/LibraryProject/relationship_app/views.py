d62ab1d74d41c22ce1ba02018e228ca25d451408
Tim <timothyochiengdev@gmail.com>
2025-02-27 20:28:56 +0300

    upper case

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,28 +20,26 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 # Role Check Functions
-def is_admin(user):
+def is_Admin(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
-def is_librarian(user):
+def is_Librarian(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
 
-def is_member(user):
+def is_Member(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Member"
 
 # Role-Based Views
 
-@user_passes_test(is_admin)
+@user_passes_test(is_Admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
 
-@user_passes_test(is_librarian)
-
+@user_passes_test(is_Librarian)
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
-@user_passes_test(is_member)
-
+@user_passes_test(is_Member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
 
