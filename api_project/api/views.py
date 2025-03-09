f67d355791675e2b6036fa5afe6d5110c5330a83
Tim <timothyochiengdev@gmail.com>
2025-03-09 18:20:27 +0300

    added bookviewset added rest framework router on url.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,15 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView 
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
 
 # Create your views here.
 class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):  # ✅ This ensures all CRUD operations are handled
     queryset = Book.objects.all()
     serializer_class = BookSerializer
