c83266abddfda760e28c302da71149c9cee5e809
Tim <timothyochiengdev@gmail.com>
2025-03-09 18:37:49 +0300

    added permission clases to bookviewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, viewsets
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 
 # ListAPIView for listing books
@@ -14,3 +14,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):  
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
