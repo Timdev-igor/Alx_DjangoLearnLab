907cf0751dd30c9255b0f78a6a4156f791bdaeb3
Tim <timothyochiengdev@gmail.com>
2025-03-23 10:36:02 +0300

    added the html,js and css files

--- django_blog/django_blog/django_blog/settings.py
+++ django_blog/django_blog/django_blog/settings.py
@@ -126,6 +126,6 @@ STATIC_URL = '/static/'
 
 # Define the static files directory explicitly
 STATICFILES_DIRS = [
-    "static",  # Looks for a 'static/' directory in the project root
+    BASE_DIR / "static", # Looks for a 'static/' directory in the project root
 ]
 
