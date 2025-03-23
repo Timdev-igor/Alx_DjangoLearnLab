fd9845a31c4ef866379c5df6204861ae7ce6ac38
Tim <timothyochiengdev@gmail.com>
2025-03-23 12:23:31 +0300

    made authentication

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
 class Post(models.Model):
     title=models.CharField(max_length=200)
     content=models.TextField()
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
+# User Login View
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')  # Redirect to user profile
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+# User Logout View
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+# User Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
