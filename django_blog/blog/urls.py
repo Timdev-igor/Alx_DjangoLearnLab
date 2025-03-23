b17860dae0b5c7ef86c488f76ddb93f9aba1556d
Tim <timothyochiengdev@gmail.com>
2025-03-23 14:50:43 +0300

    posts view urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,8 +10,13 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
 #post views///////////
     path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    # Home page: List all posts
+    # View a single post
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # Create a new post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    # Update an existing post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    # Delete a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
