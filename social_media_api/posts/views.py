b11a26c2a8d38a6b3d3b3371def01897aeb8338d
Tim <timothyochiengdev@gmail.com>
2025-03-27 17:33:38 +0300

    like

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets, permissions,generics
-from .models import Post, Comment
+from rest_framework import viewsets, permissions,generics,status
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from notifications.models import Notification 
+from rest_framework.permissions import IsAuthenticated
+ 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -33,3 +38,33 @@ class FeedView(generics.ListAPIView):
         user = self.request.user
         following_users = user.following.all()  # Fetch users the current user follow
         return Post.objects.filter(author__in=following_users).order_by('-created_at') 
+    
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+
+        if created:
+            # Create a notification when a post is liked
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                target=post
+            )
+            return Response({"message": "Post liked"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "You have already liked this post"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+
+        #  Check if the like exists
+        like = Like.objects.filter(user=request.user, post=post).first()
+        if like:
+            like.delete()
+            return Response({"message": "Like removed"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "You haven't liked this post"}, status=status.HTTP_400_BAD_REQUEST)
