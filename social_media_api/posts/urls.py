3c5c434c371fbb7eb3d58a43f9fb21f941d378df
Tim <timothyochiengdev@gmail.com>
2025-03-28 09:29:35 +0300

    fixed urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, UnlikePostView
 from .views import FeedView,LikePostView
 
 router = DefaultRouter()
