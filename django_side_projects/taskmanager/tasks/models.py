a6d08338049975c1fdd92902bd035dbc76e80ceb
Tim <timothyochiengdev@gmail.com>
2025-06-27 21:56:17 +0300

    removed

--- django_side_projects/taskmanager/tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-#created book model to test api
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
-    is_important = models.BooleanField(default=False)  # New field
