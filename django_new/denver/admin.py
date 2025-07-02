d622a50e55a0710a7ecad066a534d7fb41a92e51
Tim <timothyochiengdev@gmail.com>
2025-07-02 15:41:31 +0300

    registerd models in admin

--- django_new/denver/admin.py
+++ django_new/denver/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+#registerd models in admin -------LOG4
+from .models import Category, Photo
+
+admin.site.register(Category)
+admin.site.register(Photo)
--- django_new/denver/models.py
+++ django_new/denver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# created photo model LOG 3
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='photos/')
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
